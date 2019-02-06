f5d48a934d8b3844a74cff863cb92d894802c2fa
hoosan <hoosanalis@gmail.com>
2019-02-06 16:36:41 +0900

    initial commit

---
+++ notify.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const path = require("path");
+const request = require('request');
+const {send} = require('micro');
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+const alis = require("alis");
+const mongodb = require("mongodb");
+const DB = require("monk")(process.env.MONGODB_URL);
+const db = DB.get("alisnotify");
+
+var twitter = {};
+twitter.oauth = {
+  consumer_key: process.env.TWIBOT_TWITTER_KEY,
+  consumer_secret: process.env.TWIBOT_TWITTER_SECRET,
+  token: process.env.TWIBOT_TWITTER_TOKEN,
+  token_secret: process.env.TWIBOT_TWITTER_TOKEN_SECRET
+};
+
+var app = express();
+
+app.get("/notify", async (req, res) => {
+
+  try{
+    const docs = await db.find();
+    for (let doc of docs){
+
+      const alisId = doc.user_name;
+
+      const articles = await alis.p.users.user_id.articles.public({
+        limit: 1,
+        user_id: alisId
+      })
+
+      if (typeof articles.Items === "undefined" || articles.Items.length == 0) {
+        continue;
+      }
+
+      const sort_key_now = articles.Items[0].sort_key;
+      const url = `https://alis.to/${alisId}/articles/${articles.Items[0].article_id}`;
+      const overview = articles.Items[0].overview;
+      const title = articles.Items[0].title;
+      if (sort_key_now > doc.sort_key){
+
+        const res_update = await db.update({user_name: alisId},{$set: {sort_key: sort_key_now}});
+
+        const info = await alis.p.users.user_id.info({user_id: alisId})
+        let message = "[ALIS Notify] 新着記事\n"
+        message += `${title}\n${info.user_display_name}(ID: ${alisId})\n****\n`
+        if (overview != null){
+            message += `${overview.trim()}..\n`
+        }
+        message += `****\n${url}\n`
+
+        const followers = doc.followers;
+        for (let follower of followers){
+          await reply(follower.twitter_id, message);
+        }
+      }
+    }
+  } catch(e){
+    console.log("error in notify.js");
+    console.log(e);
+  }
+  res.sendStatus(200);
+
+})
+
+async function reply(userid, message){
+
+  // Create object
+  var message_obj = new Object();
+  message_obj = {
+    "event": {
+      "type": "message_create",
+      "message_create": {
+        "target": {
+          "recipient_id": userid
+        },
+        "message_data": {
+          "text": message
+        }
+      }
+    }
+  }
+
+  var request_options = {
+    url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
+    oauth: twitter.oauth,
+    json: true,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: message_obj
+  }
+  // send by POST
+  // request.post(request_options, function (error, response, body){});
+  await doRequest(request_options);
+
+}
+
+function doRequest(options) {
+  return new Promise(function (resolve, reject) {
+    request.post(options, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+module.exports = app;
---
+++ webhook.js
@@ -0,0 +1,252 @@
+const express = require('express');
+const path = require("path");
+const request = require('request');
+require("dotenv").config({ path: path.resolve(__dirname, ".env") })
+const bodyParser = require('body-parser');
+const monk = require('monk');
+const alis = require('alis');
+const DB = require("monk")(process.env.MONGODB_URL);
+const db = DB.get("alisnotify");
+
+const crypto = require('crypto');
+
+var twitter = {};
+twitter.oauth = {
+  consumer_key: process.env.TWIBOT_TWITTER_KEY,
+  consumer_secret: process.env.TWIBOT_TWITTER_SECRET,
+  token: process.env.TWIBOT_TWITTER_TOKEN,
+  token_secret: process.env.TWIBOT_TWITTER_TOKEN_SECRET
+};
+
+var app = express();
+
+app.get('/webhook', (req, res) => {
+
+  let crc_token = req.query.crc_token;
+  if (crc_token) {
+    let hash = crypto.createHmac('sha256', process.env.TWIBOT_TWITTER_SECRET).update(crc_token).digest('base64');
+    res.status(200);
+    res.send({
+      response_token: 'sha256=' + hash
+    })
+  } else {
+    res.status(400);
+    res.send('Error: crc_token missing from request.')
+  }
+
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/webhook", async (req, res) => {
+
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.body.direct_message_events){
+
+    // Get ID of user
+    const sender = req.body.direct_message_events[0].message_create.sender_id;
+
+    // message of DM
+    const dmMessage = req.body.direct_message_events[0].message_create.message_data.text;
+
+    if (sender != process.env.MYSELF){
+
+    　if (dmMessage.match(/解除/)){
+        await removeFollow(sender, dmMessage);
+
+      } else if (dmMessage.match(/リスト/)){
+        await checkFollowList(sender, dmMessage);
+
+      } else if (dmMessage.match(/フォロー/)){
+        await register(sender, dmMessage);
+
+      } else if (dmMessage.match(/使い方/)){
+        await howToUse(sender);
+
+      } else if (dmMessage.match(/情報/)){
+        await userArticles(sender, dmMessage);
+
+      } else {
+        const resMessage = `コマンド一覧\n\n・フォロー\n・リスト\n・フォロー解除\n・ユーザー情報\n・使い方\n`
+        await reply(sender, resMessage);
+      }
+    }
+
+  }
+  res.sendStatus(200);
+});
+
+async function register(twitterId, dmMessage){
+  const lines = dmMessage.split('\n');
+  var resMessage = "";
+
+  for (let alisId of lines) {
+
+    // 半角英数以外はスキップ
+    alisId = alisId.trim();
+    if (!(alisId.match(/[0-9a-zA-Z]/))){
+      continue;
+    }
+
+    try{
+
+      const articles = await alis.p.users.user_id.articles.public({limit: 1, user_id: alisId});
+      if (typeof articles.Items === "undefined" || articles.Items.length == 0) {
+        resMessage += `${alisId}はALIS未登録ユーザです\n`;
+      } else {
+        const docs = await db.find({user_name: alisId}, {limit:1});
+        if (docs.length > 0) {
+          if (await docs[0].followers.findIndex(({twitter_id}) => twitter_id === twitterId) >= 0){
+            resMessage += `${alisId}はフォロー済みです\n`;
+          } else {
+            resMessage += `${alisId}をフォローしました\n`
+            const obj = {twitter_id: twitterId};
+            await db.update({user_name: alisId}, {$push:{followers: obj}});
+          }
+        } else {
+          resMessage += `${alisId}をフォローしました\n`;
+          await db.insert({user_name: alisId, followers: [{twitter_id: twitterId}], sort_key: articles.Items[0].sort_key})
+        }
+      }
+
+    } catch(e) {
+      console.log("error in register function.");
+      console.log(e);
+    }
+  }
+  await reply(twitterId, resMessage);
+
+}
+
+async function checkFollowList(twitterId, dmMessage){
+
+  const lines = dmMessage.split('\n');
+  var resMessage = "";
+
+  const docs = await db.find({followers: {$elemMatch: {twitter_id: twitterId}}});
+
+  if (docs.length > 0){
+    resMessage += `フォローリスト(${docs.length}人)\n`;
+    for (let doc of docs) {
+      resMessage += `${doc.user_name}\n`;
+    }
+  } else {
+    resMessage += "誰もフォローしていません\n";
+  }
+  await reply(twitterId, resMessage);
+}
+
+async function removeFollow(twitterId, dmMessage){
+  const lines = dmMessage.split('\n');
+  var resMessage = "";
+
+  for (let alisId of lines) {
+
+    // 半角英数以外はスキップ
+    alisId = alisId.trim();
+    if (!(alisId.match(/[0-9a-zA-Z]/))){
+      continue;
+    }
+
+    const obj = {twitter_id: twitterId};
+    const res = await db.update({user_name: alisId}, {$pull:{followers: obj}});
+    if (res.nModified > 0){
+      resMessage += `${alisId}のフォローを解除しました\n`;
+    } else {
+      resMessage += `${alisId}はフォローしていません\n`;
+    }
+  }
+  await reply(twitterId, resMessage);
+}
+
+async function howToUse(twitterId){
+  const resMessage = "使い方はこちら\nhttps://alis.to/fukurou/articles/2jDXxRyO1M1x\n";
+  await reply(twitterId, resMessage);
+}
+
+async function userArticles(twitterId, dmMessage){
+  const lines = dmMessage.split('\n');
+  // var newmessage = `コマンド：ユーザ\n\n`;
+  var resMessage = "";
+
+  for (let alisId of lines) {
+
+    alisId = alisId.trim();
+    // 半角英数以外はスキップ
+    if (!(alisId.match(/[0-9a-zA-Z]/))){
+      continue;
+    }
+
+    try{
+
+      const res = await alis.p.users.user_id.articles.public({
+        limit: 1,
+        user_id: alisId
+      })
+
+      if (typeof res.Items === "undefined" || res.Items.length == 0) {
+        resMessage += `${alisId}はALIS未登録ユーザです\n`;
+      } else {
+        const info = await alis.p.users.user_id.info({user_id: alisId})
+        // console.log(JSON.stringify(info));
+        resMessage += `${info.user_display_name}\n`
+        resMessage += `id: ${alisId}\n`
+        resMessage += `${info.self_introduction}\n`
+        resMessage += `https://alis.to/users/${alisId}\n\n`
+      }
+
+    } catch(e) {
+      console.log("error in userarticles function.");
+    }
+  }
+  await reply(twitterId, resMessage);
+}
+
+async function reply(userid, message){
+
+  // Create object
+  var message_obj = new Object();
+  message_obj = {
+    "event": {
+      "type": "message_create",
+      "message_create": {
+        "target": {
+          "recipient_id": userid
+        },
+        "message_data": {
+          "text": message
+        }
+      }
+    }
+  }
+
+  var request_options = {
+    url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
+    oauth: twitter.oauth,
+    json: true,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: message_obj
+  }
+
+  // send by POST
+  await doRequest(request_options);
+
+}
+
+function doRequest(options) {
+  return new Promise(function (resolve, reject) {
+    request.post(options, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+module.exports = app
